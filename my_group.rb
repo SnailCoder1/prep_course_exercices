486499070d1882d829bf26fb17d10d5d94e3f1e2
boa <boa.matule@gmail.com>
2019-03-21 11:33:52 +0100

    refactored the code

--- my_group.rb
+++ my_group.rb
@@ -12,7 +12,9 @@
 2.3.4 :002 > my_group.each do  |my_group|
 2.3.4 :003 >      puts "#{my_group[:name]} is a  #{my_group[:age]} old #{my_group[:gender]}." 
 2.3.4 :004?>     end
+
+#Output 
+
 Thomas is a  45 old  male.
 Noel is a  34 old male.
 Mary is a  27 old female.
-=> [{:name=>"Thomas", :gender=>" male", :age=>45}, {:name=>"Noel", :gender=>"male", :age=>34}, {:name=>"Mary", :gender=>"female", :age=>27}] 
